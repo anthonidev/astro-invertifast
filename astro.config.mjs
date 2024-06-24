e2b36970ef4d102b15b6db6df5106342885700c2
anthonidev <softwaretoni21@gmail.com>
2024-06-23 22:18:35 -0500

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 
@@ -8,5 +8,6 @@ import vercel from "@astrojs/vercel/serverless";
 export default defineConfig({
   integrations: [tailwind(), icon()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
+
 });
