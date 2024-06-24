fdf0d9e593cd1dfa3d5faab242f35f27a941f0f2
anthonidev <softwaretoni21@gmail.com>
2024-06-23 22:57:17 -0500

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,10 @@
-import { defineConfig, passthroughImageService } from 'astro/config';
+import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 
 
-import vercelStatic from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
-  output: "static",
-  adapter: vercelStatic(
-    {
-      imageService: passthroughImageService(),
-    }
-  ),
-
 });
